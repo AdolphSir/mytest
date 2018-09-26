5ff97cfb10d9f36ddff82de3057e1437acb9186e
scl <m13229004458@163.com>
2018-09-26 19:12:30 +0800

    第一次上传本地项目

---
+++ demo_test/src/main/java/TestDemo.java
@@ -0,0 +1,13 @@
+/**
+ * @Author: 可乐要加冰
+ * @WeChat: SCL9205
+ * @E-mail: m13229004458@163.com
+ * @Date :  2018/09/26   19:05
+ * @param:
+ * @Description:    测试类
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("测试类...");
+    }
+}
